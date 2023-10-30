932dca4c7a0731809ebe77bdbdf5984af2d06046
Rahulkotha1 <rahul.kotha27@gmail.com>
2023-10-30 22:06:29 +0530

    first commit

---
+++ age-calculator/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import TaskFive from './Components/TaskFive';
+
+function App() {
+  return (
+    <div>
+      <TaskFive />
+    </div>
+  );
+}
+
+export default App;
